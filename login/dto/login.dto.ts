685593fd9a2cf10945e03fb9bffbadf80a6723bf
dhanushPatel112 <pateldhanush1208@gmail.com>
2023-12-06 23:11:26 +0530

    Removed unused folder

--- login/dto/login.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsStrongPassword } from 'class-validator'
-
-export class LoginDto {
-    @IsEmail({}, { message: 'Email is not valid' })
-    email: number
-
-    @IsStrongPassword()
-    password: string
-}
